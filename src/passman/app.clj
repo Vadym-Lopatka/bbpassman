d82827320a77d1d7da812dc2f1ed02132ca7e563
vlopatka <vadim.lopatka@gmail.com>
2023-05-14 14:30:49 +0300

    added listing stored pwds param

--- src/passman/app.clj
+++ src/passman/app.clj
@@ -1,4 +1,11 @@
-(ns passman.app)
+(ns passman.app
+  (:require [clojure.tools.cli :refer [parse-opts]]))
+
+(def cli-options
+   [["-l" "--list"]])
 
 (defn -main [& args]
-  (println "Hello World!"))
+  (let [parsed-options (parse-opts args cli-options)
+        options (:options parsed-options)]
+    (cond
+      (:list options) (println "The list of passwords"))))
