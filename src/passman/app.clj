44c46ee0c1b22680fc2584066a25827f35cf4c2c
vlopatka <vadim.lopatka@gmail.com>
2023-05-15 18:32:58 +0300

    added pwd gen

--- src/passman/app.clj
+++ src/passman/app.clj
@@ -1,14 +1,31 @@
 (ns passman.app
   (:require [clojure.tools.cli :refer [parse-opts]]
             [passman.db :as db]
+            [passman.password :as pwd]
             [table.core :as t]))
 
 (def cli-options
-   [["-l" "--list"]])
+   [["-l" "--length Length" "Password length"
+     :default 12
+     :parse-fn #(Integer/parseInt %)
+     :validate [#(< 0 % 100) "Must be a number between 0 and 100"]]
+    ["-g" "--generate" "Generate new password"]
+    [nil "--list"]])
 
 (defn -main [& args]
   (let [parsed-options (parse-opts args cli-options)
+        url (first (:arguments parsed-options))
+        username (second (:arguments parsed-options))
         options (:options parsed-options)]
+    
+    (println (str "url: " url " username " username))
+    (println options)
     (cond
-      (:list options) (t/table (db/get-passwords)))))
+      (:list options) (t/table (db/get-passwords))
+      (:generate options) (let [password (pwd/generate-password (:length options))]
+                            (println password)))))
+
+(comment
+  (-main)
+  )
 
---
+++ src/passman/password.clj
@@ -0,0 +1,13 @@
+(ns passman.password
+  (:require [clojure.string :as str]))
+
+(def available-chars
+  (map char (range 33 123)))
+
+(defn generate-password [length]
+  (str/join (repeatedly length #(rand-nth available-chars))))
+
+
+(comment
+  (generate-password 20)
+  )
